3392f7c9bb8ae070bfe55b5abe524d6a4ebff812
ysyheidi <ysyheidi@outlook.com>
2019-01-01 22:51:38 +0800

    automated process

--- my-site/deploy.sh
+++ my-site/deploy.sh
@@ -1,3 +1,8 @@
+rm -rf public
 hugo
+mv public/categories public/album
+cp album_index.html public/album/index.html
+rm -rf ~/Documents/ysy/ysyheidi.githuob.io/*
 cp -r public/* ~/Documents/ysy/ysyheidi.github.io
+cp album_index.html ~/Documents/ysy/ysyheidi.github.io/
 echo "generated files copied"
