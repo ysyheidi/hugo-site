ec6d7e6bf778f024a7469966b254f468b78a4297
ysyheidi <ysyheidi@outlook.com>
2019-01-02 15:00:48 +0800

    finished most parts

--- my-site/deploy.sh
+++ my-site/deploy.sh
@@ -1,8 +1,11 @@
 rm -rf public
 hugo
+# replace categories with album, replace html, feed in resources
 mv public/categories public/album
 cp album_index.html public/album/index.html
+cp album_photos/* public/album/
+# move the project to site repo
 rm -rf ~/Documents/ysy/ysyheidi.githuob.io/*
 cp -r public/* ~/Documents/ysy/ysyheidi.github.io
 cp album_index.html ~/Documents/ysy/ysyheidi.github.io/
-echo "generated files copied"
+echo "generated files copied, go to repo to push"
